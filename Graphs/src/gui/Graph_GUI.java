758df0622a734b9392ec8457c13522a3127d22b3
zohar <zohar@DESKTOP-DMKFBMS>
2019-12-18 18:33:57 +0200

    Creating gui package, crating Window class, adding menu bar and items

---
+++ Graphs/src/gui/Graph_GUI.java
@@ -0,0 +1,15 @@
+package gui;
+import javax.swing.JFrame;
+
+public class Graph_GUI {
+
+	public static void main(String[] args) 
+	{
+		Window window = new Window();
+		window.setVisible(true);
+	}
+
+	
+	
+	
+}
