8a865aa993f704f2676b7aa97fced532f3abff27
lidorT <user@DESKTOP-MQM5DDS>
2019-12-18 12:53:02 +0200

    Creating Edge class

---
+++ Graphs/src/dataStructure/Edge.java
@@ -0,0 +1,74 @@
+package dataStructure;
+
+public class Edge implements edge_data {
+	
+	public int src;
+	public int dest;
+	public int tag;
+	public double weight;
+	public String info;
+	
+	
+	public Edge(){
+		
+		this.src=0;
+		this.dest=0;
+		this.tag=0;
+		this.weight=0.0;
+		this.info = null;
+		
+	}
+	
+	public Edge(int src, int dest, double weight){
+		
+		this.src=src;
+		this.dest=dest;
+		this.weight=weight;
+		
+	
+		
+	}
+
+	
+	public int getSrc() {
+		
+		return this.src;
+	}
+
+
+	public int getDest() {
+		
+		return this.dest;
+	}
+
+
+	public double getWeight() {
+		
+		return this.weight;
+	}
+
+	
+	public String getInfo() {
+		
+		return this.info;
+	}
+
+	
+	public void setInfo(String s) {
+		this.info=s;
+		
+	}
+
+	
+	public int getTag() {
+		
+		return this.tag;
+	}
+
+	
+	public void setTag(int t) {
+		this.tag=t;
+		
+	}
+
+}
--- Graphs/src/dataStructure/node_data.java
+++ Graphs/src/dataStructure/node_data.java
@@ -4,7 +4,6 @@ import utils.Point3D;
 /**
  * This interface represents the set of operations applicable on a 
  * node (vertex) in a (directional) weighted graph.
- * @author boaz.benmoshe
  *
  */
 public interface node_data {
