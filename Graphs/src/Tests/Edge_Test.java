d86c1e3ca6bad1de7cef330426bab101323cc2a5
lidorT <user@DESKTOP-MQM5DDS>
2020-01-01 22:37:20 +0200

    Creating Edge_Test.

---
+++ Graphs/src/Tests/Edge_Test.java
@@ -0,0 +1,54 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Edge_Test {
+
+	@Test
+	public void testEdge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEdgeIntIntDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSrc() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTag() {
+		fail("Not yet implemented");
+	}
+
+}
