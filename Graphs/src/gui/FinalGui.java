93a5fc57de1a998d0c7359c5cc2c818fc43cfe48
Zohar <USER@USER-PC.lan>
2020-01-04 17:40:41 +0200

    We Decided to change all of our GUI Class From StdDraw to JFrame in order to connect buttons to functions easily

---
+++ Graphs/src/gui/FinalGui.java
@@ -0,0 +1,137 @@
+package gui;
+
+import dataStructure.DGraph;
+import dataStructure.Node;
+import dataStructure.graph;
+import utils.Point3D;
+
+public class FinalGui {
+
+	public static void main(String[] args) {
+		
+		Point3D p1 = new Point3D (720,350,10);
+		Point3D p2 = new Point3D (500,800,9);
+		Point3D p3 = new Point3D (20,60,11);
+		Point3D p4 = new Point3D (500,1000,0.4);
+		Point3D p5 = new Point3D (200,680,0.5);
+		Point3D p6 = new Point3D (800,180,0.5);
+		Point3D p7 = new Point3D (400,200,4);
+		Point3D p8 = new Point3D (480,500,0.4);
+		Point3D p9 = new Point3D (750,850,0.5);
+		Point3D p10 = new Point3D (900,700,0.5);
+		Point3D p11 = new Point3D (910,710,4);
+		Point3D p12 = new Point3D (630,950,0.4);
+		Point3D p13 = new Point3D (250,660,0.5);
+		Point3D p14 = new Point3D (710,555,0.5);
+		Point3D p15 = new Point3D (458,226,4);
+		Point3D p16 = new Point3D (811,722,0.4);
+		Point3D p17 = new Point3D (251,654,0.5);
+		Point3D p18 = new Point3D (611,230,0.5);
+		Point3D p19 = new Point3D (280,570,4);
+
+		Node n1 = new Node (1, 9, p1);
+		Node n2 = new Node (2, 5.5, p2);
+		Node n3 = new Node (3, 5.3, p3);
+		Node n4 = new Node (4, 2, p4);
+		Node n5 = new Node (5, 5.7, p5);
+		Node n6 = new Node (6,4,p6);
+		Node n7 = new Node (7,9,p7);
+		Node n8 = new Node (8, 7, p8);
+		Node n9 = new Node (9, 5.2, p9);
+		Node n10 = new Node (10, 1, p10);
+		Node n11 = new Node (11, 3.6, p11);
+		Node n12 = new Node (12, 3, p12);
+		Node n13 = new Node (13,1.5,p13);
+		Node n14 = new Node (14,3,p14);
+		Node n15 = new Node (15, 4, p15);
+		Node n16 = new Node (16, 5.6, p16);
+		Node n17 = new Node (17, 6, p17);
+		Node n18 = new Node (18, 5.8, p18);
+		Node n19 = new Node (19, 8, p19);
+
+		graph g = new DGraph();
+
+		g.addNode(n1);
+		g.addNode(n2);
+		g.addNode(n3);
+		g.addNode(n4);
+		g.addNode(n5);
+		g.addNode(n6);
+		g.addNode(n7);
+		g.addNode(n8);
+		g.addNode(n9);
+		g.addNode(n10);
+		g.addNode(n11);
+		g.addNode(n12);
+		g.addNode(n13);
+		g.addNode(n14);
+		g.addNode(n15);
+		g.addNode(n16);
+		g.addNode(n17);
+		g.addNode(n18);
+		g.addNode(n19);
+
+		g.connect(1,2,3);
+		g.connect(2,1,11);
+		g.connect(2,3,5.9);
+		g.connect(3,2,8);
+		g.connect(3,4,5.2);
+		g.connect(4,3,5);
+		g.connect(5,4,6);
+		g.connect(4,5,2.7);
+		g.connect(1,6,5);
+		g.connect(6,1,3.8);
+		g.connect(6,5,8.6);
+		g.connect(5,6,8.6);
+		g.connect(7,8,3);
+		g.connect(8,7,11);
+		g.connect(8,9,5.9);
+		g.connect(9,8,5.9);
+		g.connect(9,10,8);
+		g.connect(10,9,5.9);
+		g.connect(10,8,5.2);
+		g.connect(10,6,5);
+		g.connect(10,11,5.9);
+		g.connect(11,10,5.9);
+		g.connect(11,6,6);
+		g.connect(12,13,2.7);
+		g.connect(6,12,5);
+		g.connect(13,14,3.8);
+		g.connect(14,13,8.6);
+		g.connect(15,2,8.6);
+		g.connect(2,16,3);
+		g.connect(16,17,2);
+		g.connect(16,10,5.9);
+		g.connect(15,3,8);
+		g.connect(17,4,5.2);
+		g.connect(8,18,5);
+		g.connect(18,16,6);
+		g.connect(18,5,2.7);
+		g.connect(18,12,5);
+		g.connect(9,18,3.8);
+		g.connect(19,1,8.6);
+		g.connect(18,19,8.6);
+		g.connect(12,3,2);
+		g.connect(12,2,2);
+		g.connect(3,8,2);
+		g.connect(9,11,2);
+		g.connect(14,12,2);
+		g.connect(16,5,2);
+		g.connect(17,19,2);
+		g.connect(19,17,2);
+		g.connect(5,16,2);
+		g.connect(15,19,2);
+		g.connect(19,15,2);
+		g.connect(16,6,2);
+		g.connect(6,16,2);
+
+		//stdGui test = new stdGui();
+		
+		
+		Window Window = new Window(g);
+		
+		Window.setVisible(true);
+		//Window.repaint();
+	}
+
+}
--- Graphs/src/gui/Window.java
+++ Graphs/src/gui/Window.java
@@ -1,4 +1,5 @@
 package gui;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Menu;
@@ -6,188 +7,302 @@ import java.awt.MenuBar;
 import java.awt.MenuItem;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import dataStructure.Edge;
+import javax.swing.JOptionPane;
+import algorithms.Graph_Algo;
 import dataStructure.edge_data;
 import dataStructure.graph;
 import dataStructure.node_data;
 import utils.Point3D;
 
-public class Window extends JFrame implements ActionListener, MouseListener
+public class Window extends JFrame implements ActionListener
 {
+
+
+	private graph graph;
 	private static final long serialVersionUID = 1L;
 	LinkedList<Point3D> points = new LinkedList<Point3D>();
-	graph graph;
-	
-	public Window()
-	{
+
+	public Window(){
+		this.graph =null;
 		initGUI();
 	}
-	
-	public Window(graph graph)
+
+	public Window(graph g)
 	{
+		this.graph=g;
 		initGUI();
-		this.graph=graph;
-		getPoints(this.graph);
 	}
+
+	
+	/**
+	 * This function purpose is to initialize the GUI.
+	 * Here we set the GUI size, we creates the menu bar and adds its options.
+	 */
 	
 	
 	private void initGUI() 
 	{
-		this.setSize(1000, 850);
+		this.setSize(700, 700);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		this.setVisible(true);
+
 		MenuBar menuBar = new MenuBar();
-		Menu menu = new Menu("Menu");
-		menuBar.add(menu);
+		Menu File = new Menu("File");
+		menuBar.add(File);
+		Menu Graph_Algorithms = new Menu("Graph Algorithms");
+		menuBar.add(Graph_Algorithms);
 		this.setMenuBar(menuBar);
-		
-		MenuItem item1 = new MenuItem("Item 1");
-		item1.addActionListener(this);
-		
-		MenuItem item2 = new MenuItem("Item 2");
-		item2.addActionListener(this);
-	
-		MenuItem item3 = new MenuItem("Save");
-		item3.addActionListener(this);
-		
-		MenuItem item4 = new MenuItem("Load");
-		item4.addActionListener(this);
-		
-		menu.add(item1);
-		menu.add(item2);
-		menu.add(item3);
-		menu.add(item4);
-		
-		this.addMouseListener(this);
+
+		MenuItem save = new MenuItem("Save");
+		save.addActionListener(this);
+
+		MenuItem load = new MenuItem("Load");
+		load.addActionListener(this);
+
+		File.add(save);
+		File.add(load);
+
+		MenuItem Drawgraph = new MenuItem("Refresh graph");
+		Drawgraph.addActionListener(this);
+
+		MenuItem isConnected = new MenuItem("Is This Graph Connected");
+		isConnected.addActionListener(this);
+
+		MenuItem shortestPathDist = new MenuItem("Shortest Path Distance");
+		shortestPathDist.addActionListener(this);
+
+		MenuItem shortestPath = new MenuItem("Shortest Path");
+		shortestPath.addActionListener(this);
+
+		MenuItem TSP = new MenuItem("TSP");
+		TSP.addActionListener(this);
+
+		Graph_Algorithms.add(Drawgraph);
+		Graph_Algorithms.add(isConnected);
+		Graph_Algorithms.add(shortestPathDist);
+		Graph_Algorithms.add(shortestPath);
+		Graph_Algorithms.add(TSP);
 	}
 
+	/**
+	 * paint functions is the basic function that paints the graph as JFrame required.
+	 * we set the points(Node) and the edges inside the graph.
+	 * we set the size of the points, lines , weights and more.
+	 * 
+	 */
 	public void paint(Graphics g)
 	{
 		super.paint(g);
-		
-		for (Point3D p : points) 
-		{
-			g.setColor(Color.BLUE);
-			g.fillOval((int)p.x(), (int)p.y(), 10, 10);
-		}
-		
-		for (node_data newNode : this.graph.getV()) 
-		{
-			g.setColor(Color.RED);
-			
-			for (edge_data newEdge : this.graph.getE(newNode.getKey())) {
+
+		if(this.graph != null) {
+			Collection <node_data> Vertexes = this.graph.getV();
+			for (node_data node_data : Vertexes) {
+				Point3D TempPoint = node_data.getLocation();
+				g.setColor(Color.RED);
+				g.fillOval(TempPoint.ix(), TempPoint.iy(), 10, 10);
+				g.setColor(Color.BLACK);
+				g.drawString(Integer.toString(node_data.getKey()), (TempPoint.ix()+1), (TempPoint.iy()-2));
+				Collection<edge_data> Edge = this.graph.getE(node_data.getKey());
+				for (edge_data edge_data : Edge) {
+					if (edge_data.getTag() ==9999) {
+						edge_data.setTag(0);
+						g.setColor(Color.RED);
+					}
+					else {
 				
-				//g.drawLine(this.graph.getNode(newEdge.getSrc()).getLocation().x(), this.graph.getNode(newEdge.getSrc()).getLocation().y(), this.graph.getNode(newEdge.getDest()).getLocation().x(), this.graph.getNode(newEdge.getDest()).getLocation().y());
+						g.setColor(Color.DARK_GRAY);
+						
+					}
+					node_data dest = graph.getNode(edge_data.getDest());
+					Point3D TempPoint2 = dest.getLocation();
+					if (TempPoint2 != null) {
+						g.drawLine(TempPoint.ix(), TempPoint.iy(),
+								TempPoint2.ix(), TempPoint2.iy());
+						g.drawString(Double.toString(edge_data.getWeight()),(TempPoint.ix()+TempPoint2.ix())/2 , (TempPoint.iy()+TempPoint2.iy())/2);
+						g.setColor(Color.GREEN);
+						int XFrame =((((((TempPoint.ix()+TempPoint2.ix())/2)+TempPoint2.ix())/2)+TempPoint2.ix())/2);
+						int YFrame = ((((((TempPoint.iy()+TempPoint2.iy())/2)+TempPoint2.iy())/2)+TempPoint2.iy())/2);
+						g.fillOval(XFrame, YFrame, 6, 6);	
+					}
+				}
 			}
-			
-			
-			//g.fillOval((int).x(), (int)p.y(), 10, 10);
 		}
-		
-		
-		
-//			if(prev != null)
-//			{
-//				g.setColor(Color.RED);
-//				g.drawLine((int)p.x(), (int)p.y(), 
-//						(int)prev.x(), (int)prev.y());
-//				
-//				g.drawString("5", (int)((p.x()+prev.x())/2),(int)((p.y()+prev.y())/2));
-//			}
-			
-		//	prev = p;
-		
+	}
+
+	/**
+	 * this function simply connects the GUI button " Save "  to the Graph_Algo function save.
+	 */
+	private void Save() {
+		Graph_Algo graphalgo = new Graph_Algo();
+		graphalgo.init(this.graph);
+
+		JFrame Frame = new JFrame();
+		JFileChooser Choose = new JFileChooser();
+		Choose.setDialogTitle("Save a file");   
+		int userSelection = Choose.showSaveDialog(Frame);
+
+		if (userSelection == JFileChooser.APPROVE_OPTION) {
+			File Save = Choose.getSelectedFile();
+			String FileName= Save.getAbsolutePath();
+			graphalgo.save(FileName);		
+			System.out.println("Save as file: " + Save.getAbsolutePath());
+		}
+
+	}
+
+	/**
+	 * this function simply connects the GUI button " Load "  to the Graph_Algo function Load.
+	 */
+	private void Load() {
+		Graph_Algo graphalgo = new Graph_Algo();
+		JFrame Frame = new JFrame();
+		JFileChooser Choose = new JFileChooser();
+		Choose.setDialogTitle("Specify a file to load");   
+		int userSelection = Choose.showOpenDialog(Frame);
+
+		if (userSelection == JFileChooser.APPROVE_OPTION) {
+			File fileToLoad = Choose.getSelectedFile();
+			String file= fileToLoad.getAbsolutePath();
+			graphalgo.init(file);
+			repaint();
+			System.out.println("Load from file: " + fileToLoad.getAbsolutePath());
+		}
+	}
+
+	/**
+	 * this function simply connects the GUI button " shortestPathDist "  to the Graph_Algo function shortestPathDist.
+	 */
+	private void shortestPathDist() {
+		JFrame Frame = new JFrame();
+		String src = JOptionPane.showInputDialog(
+				null, "Please enter the source Node key");
+		String dest = JOptionPane.showInputDialog(
+				null, "Please enter your destination Node key");
+		try {
+			Graph_Algo graphalgo = new Graph_Algo();
+			graphalgo.init(this.graph);
+			double ans = graphalgo.shortestPathDist(Integer.parseInt(src), Integer.parseInt(dest));
+			String s = Double.toString(ans);	
+			JOptionPane.showMessageDialog(Frame, "The shortest distance you hat to travel is : " + s);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * This function simply connects the GUI button " shortestPath "  to the Graph_Algo function shortestPath.
+	 * after getting the shortest path we also mark the path with red for an easier view.
+	 */
+	private void shortestPath() {
+		JFrame Frame = new JFrame();
+		String s = "";
+		String src = JOptionPane.showInputDialog(
+				null, "Please enter the source Node key");
+		String dest = JOptionPane.showInputDialog(
+				null, "Please enter your destination Node key");
+		if (!src.equals(dest)) {
+			try {
+				Graph_Algo gg = new Graph_Algo();
+				gg.init(this.graph);
+				ArrayList<node_data> shortPath = new ArrayList<node_data>();
+				shortPath = (ArrayList<node_data>) gg.shortestPath(Integer.parseInt(src), Integer.parseInt(dest));
+				for (int i =0 ; i+1 < shortPath.size() ; i++) {
+					this.graph.getEdge(shortPath.get(i).getKey(), shortPath.get(i+1).getKey()).setTag(9999);
+					s+= shortPath.get(i).getKey() + "--> ";
+				}
+				s+= shortPath.get(shortPath.size()-1).getKey();
+				repaint();
+				JOptionPane.showMessageDialog(Frame, "the shortest path is: " +s);
+
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 *This function simply connects the GUI button " TSP "  to the Graph_Algo function TSP.
+	 *Here you'll have to enter each key of node you want to travel and in order to stop to enter Stop.
+	 */
+
+	private void TSP() {
+		JFrame Frame = new JFrame();
+		Graph_Algo graph_algo = new Graph_Algo();
+		graph_algo.init(this.graph);
+		ArrayList<Integer> targets = new ArrayList<Integer>();
+		String tempSrc = "";
+		String temp = "";
+		do {
+			tempSrc = JOptionPane.showInputDialog(
+					null, "Enter a key, to stop this dialog please isert the word Stop");
+			if ((!tempSrc.equals("Stop"))) {
+				targets.add(Integer.parseInt(tempSrc));
+			}
+		}
+		while(!tempSrc.equals("Stop"));
+		ArrayList<node_data> shortPath = new ArrayList<node_data>();
+		shortPath = (ArrayList<node_data>) graph_algo.TSP(targets);
+		if (shortPath != null ) {
+			for (int i =0 ; i+1 < shortPath.size() ; i++) {
+				this.graph.getEdge(shortPath.get(i).getKey(), shortPath.get(i+1).getKey()).setTag(9999);
+				temp+= shortPath.get(i).getKey() + "---> ";
+			}
+			temp+= shortPath.get(shortPath.size()-1).getKey();
+			repaint();
+			JOptionPane.showMessageDialog(Frame, "the shortest path on this graph is: " +temp);
+		}
+		if(shortPath==null) {
+			JOptionPane.showMessageDialog(Frame, "the shortest path on this graph is: null ");
+		}
 	}
 	
-	
+	private void isConnected() {
+		JFrame Frame = new JFrame();
+		Graph_Algo graph_algo = new Graph_Algo();
+		graph_algo.init(this.graph);
+		boolean ans = graph_algo.isConnected();
+		if (ans == true) {
+			JOptionPane.showMessageDialog(Frame, "this graph is connected");
+		}
+		else {
+			JOptionPane.showMessageDialog(Frame, "the graph is not connected");
+		}
+
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
-		String str = e.getActionCommand();
-		
-		if(str.equals("Item 1"))
-		{
-			Point3D p1 = new Point3D(100,100);
-			Point3D p2 = new Point3D(50,300);
-			Point3D p3 = new Point3D(400,150);
-			
-			points.add(p1);
-			points.add(p2);
-			points.add(p3);
-			
-			repaint();
+		String input = e.getActionCommand();
+
+		switch(input) {
+		case "Refresh graph" : repaint();
+		break;
+		case "Save": Save();
+		break;
+		case "Load": Load();
+		break;
+		case "Is This Graph Connected": isConnected();
+		break;
+		case "Shortest Path Distance": shortestPathDist();
+		break;
+		case "Shortest Path": shortestPath();
+		break;
+		case "TSP": TSP();
+		break;
+		default:
+			break;
+
 		}
-		
-		if(str.equals("Save"))
-		{
-			System.out.println("Lunch Save to file function");
-			// TODO saveToFile()
-			repaint();
-		}
-		
-		if(str.equals("Load"))
-		{
-			System.out.println("Lunch init from file function");
-			// TODO initFromFile()
-			repaint();
-		}
-		
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		//System.out.println("mouseClicked");
-		
-	}
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-//		int x = e.getX();
-//		int y = e.getY();
-//		Point3D p = new Point3D(x,y);
-//		points.add(p);
-//		repaint();
-//		System.out.println("mousePressed");
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		//System.out.println("mouseReleased");
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		//System.out.println("mouseEntered");
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		//System.out.println("mouseExited");
-	}
-	
-	/////// Private Methods /////////////
-	
-	
-	private void getPoints (graph g){
-		
-		Collection<node_data> nodes = g.getV();
-		
-		for (node_data node : nodes) 
-		{
-			this.points.add(node.getLocation());
-		}
-			
-	}
-	
-	
-	
 }
